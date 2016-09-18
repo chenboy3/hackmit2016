9d07c4095f78d94e1d69023cbbe382ab8b77bdbe
quinnwu <wu.quinn@gmail.com>
2016-09-17 21:51:35 -0400

    Revert "blowing it"
    This reverts commit a91b1996bb63b8dcd6047a5fb430a93a7095c7b6.

--- src/js/index.jsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import AppContainer from './components/AppContainer.jsx';
-
-ReactDOM.render(<AppContainer />, document.getElementById('main'));
